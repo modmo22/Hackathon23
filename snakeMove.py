b572f73044d9e4b1cdf34d111b809987a6b10b33
GilVandegrift <124904843+GilVandegrift@users.noreply.github.com>
2023-04-15 11:59:33 -0500

    Add files via upload
    added jump animation and capabilities to the snake

--- snakeMove.py
+++ snakeMove.py
@@ -15,6 +15,9 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 SNHEIGHT = 40
 SNWIDTH = 40
+JUMPHEIGHT = 30#constant for how hight snek gets
+JUMPVEL = 5
+
 
 ground0 = pygame.Rect(200, 350, 200, 5)
 ground = [ground0]
@@ -43,13 +46,31 @@ staticSnake2 = pygame.transform.scale(staticSnake2,(SNWIDTH, SNHEIGHT))
 staticSnake3 = pygame.image.load(os.path.join('sprites', 'staticSnake3.png'))
 staticSnake3 = pygame.transform.scale(staticSnake3,(SNWIDTH, SNHEIGHT))
 
+snJump1 = pygame.image.load(os.path.join('sprites', 'snJump1.png'))
+snJump1 = pygame.transform.scale(snJump1, (SNWIDTH, SNHEIGHT))
+snJump2 = pygame.image.load(os.path.join('sprites', 'snJump2.png'))
+snJump2 = pygame.transform.scale(snJump2, (SNWIDTH, SNHEIGHT))
+snJump3 = pygame.image.load(os.path.join('sprites', 'snJump3.png'))
+snJump3 = pygame.transform.scale(snJump3, (SNWIDTH, SNHEIGHT))
+snJump4 = pygame.image.load(os.path.join('sprites', 'snJump4.png'))
+snJump4 = pygame.transform.scale(snJump4, (SNWIDTH, SNHEIGHT))
+snJump5 = pygame.image.load(os.path.join('sprites', 'snJump5.png'))
+snJump5 = pygame.transform.scale(snJump5, (SNWIDTH, SNHEIGHT))
+snJump6 = pygame.image.load(os.path.join('sprites', 'snJump6.png'))
+snJump6 = pygame.transform.scale(snJump6, (SNWIDTH, SNHEIGHT))
+snJump7 = pygame.image.load(os.path.join('sprites', 'snJump7.png'))
+snJump7 = pygame.transform.scale(snJump7, (SNWIDTH, SNHEIGHT))
+snJump8 = pygame.image.load(os.path.join('sprites', 'snJump8.png'))
+snJump8 = pygame.transform.scale(snJump8, (SNWIDTH, SNHEIGHT))
+
+snJump = [snJump1, snJump2, snJump3, snJump4, snJump5, snJump6, snJump7, snJump8]
 rightMove = [snakeRight1, snakeRight2, snakeRight3, snakeRight2]#though not created first snakeright2 looks best as a standing still
 leftMove = [snakeLeft1, snakeLeft2, snakeLeft3, snakeLeft2]
 staticSn = [staticSnake1, staticSnake2, staticSnake3, staticSnake2]
 
 
 
-def drawWindow(snek, rightMove, rightMoveInd, direction, leftMove, leftMoveInd, staticSn, staticSnInd):
+def drawWindow(snek, rightMove, rightMoveInd, direction, leftMove, leftMoveInd, staticSn, staticSnInd, snJump, snJumpInd):
     WIN.fill(WHITE)
     for i in range(0,8):
         for j in range(0, 8):
@@ -59,15 +80,19 @@ def drawWindow(snek, rightMove, rightMoveInd, direction, leftMove, leftMoveInd, 
         WIN.blit(rightMove[rightMoveInd], (snek.x, snek.y))
     elif direction == 'left':
         WIN.blit(leftMove[leftMoveInd], (snek.x, snek.y))
+    elif direction == 'up':
+        WIN.blit(snJump[snJumpInd], (snek.x, snek.y))
     else:
         WIN.blit(staticSn[staticSnInd], (snek.x, snek.y))
     pygame.display.update()
 
 def main():
+    curJumpHeight = 0#variable for how much left the snek has to jump
     direction = 'none'
     rightMoveInd = 0
     leftMoveInd = 0
     staticSnInd = 0
+    snJumpInd = 0
     clock = pygame.time.Clock()
     run = True
     startTime = pygame.time.get_ticks()
@@ -78,12 +103,19 @@ def main():
                 run = False
                 pygame.quit()
 
-        if snek.collidelist(ground) == -1:
+        if curJumpHeight != 0:
+            curJumpHeight -= JUMPVEL
+            snek.y -= JUMPVEL
+        elif snek.collidelist(ground) == -1:
             snek.y += 1
 
         keysPressed = pygame.key.get_pressed()
         if (keysPressed[pygame.K_w] or keysPressed[pygame.K_SPACE]) and snek.collidelist(ground) != -1:
-            snek.y -= 20
+            curJumpHeight = JUMPHEIGHT
+            for i in range(0,8):
+                drawWindow(snek, rightMove, rightMoveInd, 'up', leftMove, leftMoveInd, staticSn, staticSnInd, snJump, snJumpInd)
+                snJumpInd = (snJumpInd + 1)% 8
+                pygame.time.delay(20)
 
         if keysPressed[pygame.K_d]:
             direction = 'right'
@@ -106,7 +138,7 @@ def main():
                 staticSnInd = (staticSnInd + 1) % 4
                 startTime = currentTime
 
-        drawWindow(snek, rightMove, rightMoveInd, direction, leftMove, leftMoveInd, staticSn, staticSnInd)
+        drawWindow(snek, rightMove, rightMoveInd, direction, leftMove, leftMoveInd, staticSn, staticSnInd, snJump, snJumpInd)
 
 if __name__ == "__main__":
     main()
