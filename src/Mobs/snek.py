1953e11b62b89068f5e69da8f3ca8790631a78ff
Brady Holland <oskiekid@gmail.com>
2023-04-15 19:33:04 -0500

    Move files added to root dir

--- src/Mobs/snek.py
+++ src/Mobs/snek.py
@@ -1,5 +1,58 @@
+import pygame
+
+
 from Abstract.mob import Mob
 class Snek(Mob):
     def __init__(self):
+        self._JUMPHEIGHT = 30
         super().__init__()
-        print(__file__)
+        # print(__file__)
+
+        self.image = pygame
+
+
+    def move(keys_pressed):
+        """controls the movement of the snake by receiving key presses from the level module."""
+        if (keys_pressed[pygame.K_w] or keys_pressed[pygame.K_SPACE]) and snek.collidelist(ground) != -1:
+            snCrouchInd = 0
+            curJumpHeight = self._JUMPHEIGHT
+            for i in range(0,8):
+                # I'm pretty sure this is going to be a method in the Springboard class. Import springboard
+                # drawWindow(snek, rightMove, rightMoveInd, 'up', leftMove, leftMoveInd, staticSn, staticSnInd, snJump, snJumpInd, snCrouch, snCrouchInd)
+                snJumpInd = (snJumpInd + 1)% 8
+                pygame.time.delay(20)
+
+        if keys_pressed[pygame.K_d]:
+            snCrouchInd = 0
+            direction = 'right'
+            snek.x += 2
+            currentTime = pygame.time.get_ticks()
+            if currentTime - startTime >= 100:
+                rightMoveInd = (rightMoveInd + 1) % 4
+                startTime = currentTime
+        elif keys_pressed[pygame.K_a]:
+            snCrouchInd = 0
+            direction = 'left'
+            snek.x -= 2
+            currentTime = pygame.time.get_ticks()
+            if currentTime - startTime >= 100:
+                leftMoveInd = (leftMoveInd + 1) % 4
+                startTime = currentTime
+        elif keys_pressed[pygame.K_s]:
+            direction = 'down'
+            currentTime = pygame.time.get_ticks()
+            if currentTime - startTime >= 100:
+                if snCrouchInd != 3:
+                    snCrouchInd = (snCrouchInd + 1) % 4
+                startTime = currentTime
+        else:
+            snCrouchInd = 0
+            direction = 'none'
+            currentTime = pygame.time.get_ticks()
+            if currentTime - startTime >= 166:
+                staticSnInd = (staticSnInd + 1) % 4
+                startTime = currentTime
+        
+
+    def run():
+        """Runs the game and controls the level flow."""
--- src/Stages/MainMenu.py
+++ src/Stages/MainMenu.py
@@ -1,3 +1,50 @@
+import pygame
+import os
 class MainMenu():
-    def __init__(self):
-        pass
+    def __init__(self, WIN):
+        self.WIN = WIN
+        self.BUTTWIDTH = 100
+        self.BUTTHEIGHT = 100
+
+    def draw(self):
+        TITLEWIDTH = 600
+        TITLEHEIGHT = 100
+        Title = pygame.image.load(os.path.join('sprites', 'Title.png'))
+        Title = pygame.transform.scale(Title, (TITLEWIDTH, TITLEHEIGHT))
+        BUTT1 = pygame.image.load(os.path.join('sprites', 'Butt1.png'))#unfinished
+        BUTT1 = pygame.transform.scale(BUTT1, (self.BUTTWIDTH, self.BUTTHEIGHT))
+        self.button1 = pygame.Rect(300, 300, self.BUTTWIDTH, self.BUTTHEIGHT)
+        BUTT2 = pygame.image.load(os.path.join('sprites', 'Butt2.png'))#unfinished
+        BUTT2 = pygame.transform.scale(BUTT2, (self.BUTTWIDTH, self.BUTTHEIGHT))
+        self.button2 = pygame.Rect(700, 300, self.BUTTWIDTH, self.BUTTHEIGHT)
+        BUTT3 = pygame.image.load(os.path.join('sprites', 'Butt3.png'))#unfinished
+        BUTT3 = pygame.transform.scale(BUTT3, (self.BUTTWIDTH, self.BUTTHEIGHT))
+        self.button3 = pygame.Rect(700, 300, self.BUTTWIDTH, self.BUTTHEIGHT)
+        BUTT4 = pygame.image.load(os.path.join('sprites', 'Butt4.png'))#unfinished
+        BUTT4 = pygame.transform.scale(BUTT4, (self.BUTTWIDTH, self.BUTTHEIGHT))
+        self.button4 = pygame.Rect(700, 700, self.BUTTWIDTH, self.BUTTHEIGHT)
+        BACKGROUND = pygame.image.load(os.path.join('sprites','TitleScreen.png'))#place holder image for now
+        self.WIN.blit(BACKGROUND, (0,0))
+        self.WIN.blit(Title, (675 - (TITLEWIDTH//2), 100))
+        self.WIN.blit(BUTT1, (self.button1.x, self.button1.y))
+        self.WIN.blit(BUTT2, (self.button2.x, self.button2.y))
+        self.WIN.blit(BUTT1, (self.button3.x, self.button3.y))
+        self.WIN.blit(BUTT1, (self.button4.x, self.button4.y))
+
+    def run(self):
+        #window is 1350 by 750
+        end = 0
+        while(end == 0):
+            self.draw()
+            pygame.event.get()
+            mousePos = pygame.mouse.get_pos()
+            mouseState = pygame.mouse.get_pressed()
+            if mouseState[0] == True:
+                if (mousePos[0] >= self.button1.x and mousePos[0] <= (self.button1.x + self.BUTTWIDTH)) and (mousePos[1] >= self.button1.y and mousePos[1] <= (self.button1.y + self.BUTTHEIGHT)):
+                    return 1
+                elif (mousePos[0] >= self.button2.x and mousePos[0] <= (self.button2.x + self.BUTTWIDTH)) and (mousePos[1] >= self.button2.y and mousePos[1] <= (self.button2.y + self.BUTTHEIGHT)):
+                    return 2
+                elif (mousePos[0] >= self.button3.x and mousePos[0] <= (self.button3.x + self.BUTTWIDTH)) and (mousePos[1] >= self.button3.y and mousePos[1] <= (self.button3.y + self.BUTTHEIGHT)):
+                    return 3
+                elif (mousePos[0] >= self.button4.x and mousePos[0] <= (self.button4.x + self.BUTTWIDTH)) and (mousePos[1] >= self.button4.y and mousePos[1] <= (self.button4.y + self.BUTTHEIGHT)):
+                    return 4
